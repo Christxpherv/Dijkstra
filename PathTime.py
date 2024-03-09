7efd83021978e74b86c2a58d9836b9da8358ac5e
Christopher Valle <christxpherv@gmail.com>
2024-03-08 20:41:46 -0600

    started working on the Djkstra algorithm

---
+++ PathTime.py
@@ -0,0 +1,10 @@
+# implement the Dijkstra algorithm to find the shortest path between two nodes in the graph
+class Dijkstra:
+  # initialize method that requires a graph of nodes
+  def __init__(self, graph):
+      self.graph = graph
+  
+  # find the shortest path between two nodes in the graph using the Dijkstra algorithm
+  def shortestPath(self, start_name, end_name):
+     # work in progress
+     return None 
