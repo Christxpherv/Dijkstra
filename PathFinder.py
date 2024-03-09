255290ea51a4f155729d0c6a09911d9a5e937cb4
Christopher Valle <christxpherv@gmail.com>
2024-03-09 02:11:39 -0600

    added for loop that iterates through the entire schedule and calls the Dijsktra algorithm to find the total time and the path

--- PathFinder.py
+++ PathFinder.py
@@ -12,5 +12,20 @@ class SchedulePathFinder:
       totalTime = 0
       path = []
 
-      # work in progress
-      return None
+      # iterate through the schedule to find the shortest path between each pair of buildings
+      for i in range(len(schedule) - 1):
+          # set the starting building to i
+          startBuilding = schedule[i]
+          # set the ending building to the one after the starting building
+          endBuilding = schedule[i + 1]
+
+          # find the shortest time between the starting and ending buildings using the Dijkstra algorithm
+          time = self.dijkstra.shortestPath(startBuilding, endBuilding)
+          if time is None:
+              print(f"No path found between {startBuilding} and {endBuilding}.")
+              return None
+          # add the time to the total time and append the path to the list
+          totalTime += time
+          path.append((startBuilding, endBuilding, time))
+
+      return path, totalTime
