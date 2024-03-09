f6590dd9f2e4234b0e263fc663ee7d36d06a1dba
Christopher Valle <christxpherv@gmail.com>
2024-03-08 21:49:03 -0600

    created while loop that iterates through the unvisited nodes and finds the shortest time to reach destination

--- PathTime.py
+++ PathTime.py
@@ -12,5 +12,25 @@ class Dijkstra:
       shortest_times[start_name] = 0
       # set of unvisited nodes
       unvisited = set(self.graph.nodes.keys())
+      
+      # iterate through the unvisited nodes until all nodes are visited
+      while unvisited:
+          # find the unvisited node with the shortest time
+          current_node_name = min(unvisited, key=lambda node: shortest_times[node])
+          current_node = self.graph.nodes[current_node_name]
 
-      return None
+          # break the loop if the current node is unreachable or the end node is reached
+          if shortest_times[current_node_name] == float('infinity') or current_node_name == end_name:
+              break
+          # iterate through the adjacent nodes of the current node
+          for adjacent, (edgeDistance, edgeTime) in current_node.adjacents.items():
+              # add the edge time to the shortest time of the current node
+              time = shortest_times[current_node_name] + edgeTime
+             
+              # update the shortest time of the adjacent node if the new time is shorter
+              if time < shortest_times[adjacent.name]:
+                  shortest_times[adjacent.name] = time
+          # remove the current node from the unvisited set
+          unvisited.remove(current_node_name)
+      # return the shortest time of the end node if it is reachable, else return None
+      return shortest_times[end_name] if shortest_times[end_name] != float('infinity') else None
